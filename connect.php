4cbd3e185ae341f7320b4c9807bc96e1b5a33ca0
Thai <vanthai3108@gmail.com>
2021-06-09 11:33:31 +0700

    thai

--- connect.php
+++ connect.php
@@ -11,13 +11,10 @@ $dbname ="mydatabase";
 
 $connect = mysqli_connect($server, $username, $password, $dbname);
 if (!$connect) {
-    echo "no";
 	die("Connect Failed:".mysqli_connect_error());
 	# code...
 }
-else {
-    echo "yes";
-}
+
 
 
 ?>
